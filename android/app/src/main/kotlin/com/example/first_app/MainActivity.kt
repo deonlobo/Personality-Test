390245dbf5087aaaaa055a66e4a32a5c122893ff
Dalon Lobo <dalonlobo@gmail.com>
2020-09-30 20:10:27 +0530

    Update MainActivity.kt
    launching a URL in the mobile platform

--- android/app/src/main/kotlin/com/example/first_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/first_app/MainActivity.kt
@@ -2,5 +2,10 @@ package com.example.first_app
 
 import io.flutter.embedding.android.FlutterActivity
 
+// Following 2 packages will help in launching a URL in
+// the mobile platform
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
 class MainActivity: FlutterActivity() {
 }
